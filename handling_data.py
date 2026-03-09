56d87173940f6258c5f65e783b216bc9ea493701
Nowak0 <kuubanowak@gmail.com>
2026-03-09 09:54:06 +0100

    new dataset & improvements

--- handling_data.py
+++ handling_data.py
@@ -1,22 +1,22 @@
 import numpy as np
 import pandas as pd
 
-
 TRAIN_PERCENTAGE = 0.8
 
 def load_data():
-    metrics = pd.read_csv("dataset/4768977_timeseries_data.csv")
-    bool_columns = metrics.select_dtypes(include=['bool']).columns
-    metrics[bool_columns] = metrics[bool_columns].astype(int)
-    feature_columns = metrics.select_dtypes(include=['int64', 'float64']).columns
+    metrics = pd.read_parquet("dataset/1/20.parquet")
+    feature_columns = ['p0_power_avg', 'gpu0_core_temp_avg', 'gpu0_mem_temp_avg', 'fan0_0_avg']
+    # feature_columns = ['gpu0_core_temp_avg', 'gpu0_mem_temp_avg']
+    # feature_columns = metrics.select_dtypes(include=[np.number]).columns
 
     X = metrics[feature_columns].copy()
     X = X.fillna(0)
-    y = metrics['alert_summary_status_general'].map({'TP': 1, 'SP': 1, 'TN': 0})
+    y = metrics['value']
+    y = y.fillna(0)
 
     return X, y
 
-def create_samples(X, y, past_steps=12, future_steps=5):
+def create_samples(X, y, past_steps=5, future_steps=3):
     X_windows = []
     y_windows = []
     for i in range(past_steps, X.shape[0] - future_steps + 1):
@@ -26,11 +26,10 @@ def create_samples(X, y, past_steps=12, future_steps=5):
 
     X_windows = np.array(X_windows)
     y_windows = np.array(y_windows)
-
     train_sample_length = int(TRAIN_PERCENTAGE * X_windows.shape[0])
+
     X_train = X_windows[:train_sample_length]
     y_train = y_windows[:train_sample_length]
     X_test = X_windows[train_sample_length:]
     y_test = y_windows[train_sample_length:]
     return (X_train, y_train), (X_test, y_test)
-
