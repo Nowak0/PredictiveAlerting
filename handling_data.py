4c82d5fab1bde55f97b0c6b21cfb9f95e61e7232
Nowak0 <kuubanowak@gmail.com>
2026-03-09 21:48:36 +0100

    added a new model, made improvements

--- handling_data.py
+++ handling_data.py
@@ -5,31 +5,31 @@ TRAIN_PERCENTAGE = 0.8
 
 def load_data():
     metrics = pd.read_parquet("dataset/1/20.parquet")
-    feature_columns = ['p0_power_avg', 'gpu0_core_temp_avg', 'gpu0_mem_temp_avg', 'fan0_0_avg']
-    # feature_columns = ['gpu0_core_temp_avg', 'gpu0_mem_temp_avg']
+    feature_columns = ['total_power_avg', 'gpu0_core_temp_avg', 'gpu0_mem_temp_avg', 'fan0_0_avg']
     # feature_columns = metrics.select_dtypes(include=[np.number]).columns
 
     X = metrics[feature_columns].copy()
-    X = X.fillna(0)
     y = metrics['value']
+    X = X.fillna(0)
     y = y.fillna(0)
 
     return X, y
 
 def create_samples(X, y, past_steps=5, future_steps=3):
-    X_windows = []
-    y_windows = []
+    X_batches = []
+    y_batches = []
     for i in range(past_steps, X.shape[0] - future_steps + 1):
-        X_windows.append(X.iloc[i-past_steps:i].values.flatten())
-        y_window = y.iloc[i:i+future_steps].values
-        y_windows.append(1 if y_window.sum() > 0 else 0)
+        X_batches.append(X.iloc[i-past_steps:i].values.flatten())
+        y_batch = y.iloc[i:i+future_steps].values
+        y_batches.append(1 if y_batch.sum() > 0 else 0)
 
-    X_windows = np.array(X_windows)
-    y_windows = np.array(y_windows)
-    train_sample_length = int(TRAIN_PERCENTAGE * X_windows.shape[0])
+    X_batches = np.array(X_batches)
+    y_batches = np.array(y_batches)
+    train_sample_length = int(TRAIN_PERCENTAGE * X_batches.shape[0])
 
-    X_train = X_windows[:train_sample_length]
-    y_train = y_windows[:train_sample_length]
-    X_test = X_windows[train_sample_length:]
-    y_test = y_windows[train_sample_length:]
+    X_train = X_batches[:train_sample_length]
+    y_train = y_batches[:train_sample_length]
+    X_test = X_batches[train_sample_length:]
+    y_test = y_batches[train_sample_length:]
+
     return (X_train, y_train), (X_test, y_test)
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from handling_data import load_data, create_samples
-from random_forest import run_advanced_random_forest, run_basic_random_forest
+from models import run_random_forest, run_xgboost
 
 
 def main():
     X, y = load_data()
     train_data, test_data = create_samples(X, y)
-    accuracy1, recall1, precision1 = run_basic_random_forest(*train_data, *test_data)
+
+    accuracy1, recall1, precision1 = run_random_forest(*train_data, *test_data)
     print(accuracy1, recall1, precision1)
-    # accuracy2, recall2, precision2 = run_advanced_random_forest(*train_data, *test_data, X)
-    # print(accuracy2, recall2, precision2)
+
+    accuracy2, recall2, precision2 = run_xgboost(*train_data, *test_data)
+    print(accuracy2, recall2, precision2)
 
 
 if __name__ == "__main__":
